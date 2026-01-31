7af59912e7e9548fb2d9858ad53327fae09cd0e3
nithya <tmnithya981@gmail.com>
2026-01-31 15:15:14 +0530

    commit

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template
+import os
 
 app = Flask(__name__)
 
@@ -16,6 +17,5 @@ def abc():
     return render_template("abc.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port,debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
