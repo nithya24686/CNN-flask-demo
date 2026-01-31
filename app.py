97abbb920c2d17476e8987561e7a27b8bd07942d
nithya <tmnithya981@gmail.com>
2026-01-31 15:06:47 +0530

    commit

--- app.py
+++ app.py
@@ -3,10 +3,19 @@ from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route("/")
-def index():
+def hello_world():
     return render_template("index.html")
+
+@app.route("/<name>")
+def name(name):
+    return render_template("name.html",name=name,image=name+".png")
+
+
 @app.route("/abc")
 def abc():
     return render_template("abc.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port,debug=True)
