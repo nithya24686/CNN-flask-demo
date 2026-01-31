5565230a3a22c681a29226b3715d9fa0dddd234d
nithya <tmnithya981@gmail.com>
2026-01-31 12:34:03 +0530

    first commit

--- templates/app.py
+++ templates/app.py
@@ -3,10 +3,16 @@ from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route("/")
-def index():
+def hello_world():
     return render_template("index.html")
+
+@app.route("/<name>")
+def name(name):
+    return render_template("name.html")
+
 @app.route("/abc")
 def abc():
     return render_template("abc.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
